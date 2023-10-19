366037d83d7d75f89eebe49867fa7c5904505baa
chinnapareddy <chinna.aduri@gmail.com>
2023-10-18 19:40:18 -0700

    terraform structure

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "components" {
-  source = "git::https://github.com/chinna3107/p1-tf-module-vpc.git"
-  for_each = var.vpc
-  cidr = each.value["cidr"]
-  subnet = each.value["subnet"]
+module "vpc" {
+  source     = "git::https://github.com/chinna3107/p1-tf-module-vpc.git"
+  for_each   = var.vpc
+  cidr       = each.value["cidr"]
+  subnet     = each.value["subnet"]
 }
