8089dbc453a7a91ddd15d3e2980852f67ea49626
chinnapareddy <chinna.aduri@gmail.com>
2023-10-21 07:02:02 -0700

    terraform structure

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   vpc_id      = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   # app_subnets = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app" , null), "subnet_ids", null): v.id]
-  app_subnets = lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app" , null)
+  app_subnets = lookup(lookup(lookup(module.vpc, "main", null), "subnet", null), "app" , null)
     }
