d30dee0deeb739bd5951ff22186b9b85e117f88e
chinnapareddy <chinna.aduri@gmail.com>
2023-10-22 00:41:53 -0700

    AWS Parameters

--- main.tf
+++ main.tf
@@ -30,8 +30,14 @@ module "docdb" {
    tags                       = var.tags
   env                        = var.env
 
-  for_each   = var.docdb
-  subnet_ids = local.db_subnets
+  for_each                = var.docdb
+  subnet_ids              = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  vpc_id                  = local.vpc_id
+  sg_ingress_cidr         = local.app_subnets_cidr
+
 }
 
 
