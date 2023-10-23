21943a1d3a1654d46c2c3d048914df66e80ef1bb
chinnapareddy <chinna.aduri@gmail.com>
2023-10-23 06:07:19 -0700

    Terraform Elasticache

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ module "rds" {
 
 
 module "elasticache" {
-  source = "git::https://github.com/raghudevopsb74/tf-module-elasticache.git"
+  source = "git::https://github.com/chinna3107/p1-tf-module-elasticache.git"
   tags   = var.tags
   env    = var.env
 
