1dcc5d9e4b1d00be8a308d00413a895f4ba62ccc
chinnapareddy <chinna.aduri@gmail.com>
2023-11-18 03:55:58 -0800

    Terraform App

--- main.tf
+++ main.tf
@@ -106,6 +106,7 @@ module "rabbitmq" {
 
 
 module "app" {
+  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
   source = "git::https://github.com/chinna3107/p1-tf-module-app.git"
 
   tags             = var.tags
