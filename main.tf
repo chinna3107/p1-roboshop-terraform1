feaa7ac165eda780e67c8a24090b7b2cc1d0a729
chinnapareddy <chinna.aduri@gmail.com>
2023-11-28 21:20:48 -0800

    Roboshop prometheus

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ module "alb" {
 
 }
 
+/*
 
 module "docdb" {
   source     = "git::https://github.com/chinna3107/p1-tf-module-docdb.git"
@@ -103,10 +104,10 @@ module "rabbitmq" {
   ssh_ingress_cidr = var.ssh_ingress_cidr
 
 }
-
+*/
 
 module "app" {
-  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
+  # depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
   source = "git::https://github.com/chinna3107/p1-tf-module-app.git"
 
   tags             = var.tags
